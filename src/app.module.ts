5ff1b38147df9a89900c5078f8eeef92537cd3af
DEV-LEE113 <devlee113@gmail.com>
2025-02-04 15:04:04 +0900

    Mongo 실 서비스 URL 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { LogsModule } from './tracking/log.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/tracking'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/tracking'),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     LogsModule,
   ],
+  providers: [],
 })
 export class AppModule {}
